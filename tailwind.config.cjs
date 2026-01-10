1fffa41c3842ce1217f4c41835d202e1279a901c
tungnt <thanhtung3523@gmail.com>
2026-01-10 13:37:43 +0700

    ui: product cmt, product accordion

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,15 @@ module.exports = {
 			fontFamily: {
 				nunito: ['Nunito', 'sans-serif'],
 			},
+			keyframes: {
+				scroll: {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(-50%)' },
+				},
+			},
+			animation: {
+				scroll: 'scroll 10s linear infinite',
+			},
 		},
 	},
 	plugins: [],
